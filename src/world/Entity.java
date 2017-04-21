85a7b78005f6e958852435446f758ebc844cf01a
dayyad <ease.spam@gmail.com>
2017-04-22 02:28:36 +1200

    nade collidable back to interface

--- src/world/Entity.java
+++ src/world/Entity.java
@@ -1,16 +1,23 @@
 package world;
 
-public class Entity {
+/**
+ * @author Vo
+ *
+ *Any object that is represented as a physical object in the game.
+ *
+ */
+public abstract class Entity {
 	
-
+	
 	public Entity(){
 		//Placeholder
 	}
+	
 	public int getX(){
 		return 0;
 	}
+	
 	public int getY(){
 		return 0;
 	}
-	
 }
--- src/world/World.java
+++ src/world/World.java
@@ -1,7 +1,10 @@
 package world;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import events.Event;
 import main.Observerable;
@@ -10,8 +13,41 @@ import main.Subject;
 public class World extends Subject implements Observerable {
 	//private Player player;
 	
+	private List<Entity> entities = new ArrayList<>();
+	
 	public World(){
 		super();
+	}
+	
+	/**
+	 * @return gets entities that implement collidable
+	 */
+	
+	public Collection<Entity> getCollidableEntities(){
+		List<Entity> returnList = new ArrayList<>();
+		
+		for(Entity e : this.entities){
+			if(e instanceof physics.Collidable){
+				returnList.add(e);
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * @return gets entities that implement renderable
+	 */
+	public Collection<Entity> getRenderableEntities(){
+		List<Entity> returnList = new ArrayList<>();
+		
+		for(Entity e : this.entities){
+			if(e.getClass().isInstance(Renderable.Renderable)){
+				returnList.add(e);
+			}
+		}
+		
+		return null;
 	}
 	
 	/**
@@ -26,7 +62,7 @@ public class World extends Subject implements Observerable {
 	 * Gets the collision map for the currently loaded map
 	 * @return
 	 */
-	public Image getCollisionMap(){
+	public Color[][] getCollisionMap(){
 		return null;
 	}
 	
