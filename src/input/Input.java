cd23ae53cb8089c88122889765bc7d10d9da4d1e
mgoo <andrewmcghienz@gmail.com>
2017-04-22 14:17:33 +1200

    updated some small things in input

--- src/input/Input.java
+++ src/input/Input.java
@@ -5,38 +5,64 @@ import java.awt.event.MouseEvent;
 
 import events.Event;
 import events.Events;
+import main.Globals;
 import main.Subject;
+import main.UserActions;
 import physics.Move;
 import physics.Physics;
 import world.World;
 
-public class Input extends Subject{
-	//LISTERns
-	private final Physics physics;
+public class Input extends UserActions{
+	private Subject subject;
 	
 	public Input(Physics physics){
 		super();
-		this.physics = physics;
-	}
-	
-	public void onKeyDown(KeyEvent e){
-		// Handle Key Down Event
-	}
-	
-	public void onKeyUp(KeyEvent e){
-		// Handle Key up event
-	}
-	
-	public void onMouseMove(MouseEvent e){
-		// Handle Mouse Move event
-	}
-	
-	public void onMouseClick(MouseEvent e){
-		// Handle Mouse click event
+		this.subject = new Subject() {
+			@Override
+			protected void initObservers() {
+				this.addObservers(physics);
+			}
+		};
 	}
 
 	@Override
-	protected void initObservers() {
-		this.addObservers(this.physics);
+	public void keyTyped(KeyEvent e) {
+		System.out.println("Recived keyTyped");
+		subject.notify(Events.moveEvent(new Move(Globals.player, 10, 10)));
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
 	}
 }
