3727e380e9405557ffbc089cbe308e59d3e103d0
mgoo <andrewmcghienz@gmail.com>
2017-04-23 10:32:12 +1200

    added looping to the sounds and footsteps

--- src/sound/SoundManager.java
+++ src/sound/SoundManager.java
@@ -4,7 +4,10 @@ package sound;
 import ecs100.Sound;
 import events.Event;
 import main.Observerable;
+import physics.Move;
+import world.Player;
 
+import javax.sound.sampled.LineEvent;
 import java.io.IOException;
 
 /**
@@ -12,6 +15,10 @@ import java.io.IOException;
  */
 public class SoundManager implements Observerable{
     private Sound background;
+    private Sound footsteps;
+    private Sound gunshot;
+
+    private boolean playing_footsteps;
 
     public SoundManager(){
         this.loadFiles();
@@ -19,15 +26,46 @@ public class SoundManager implements Observerable{
 
     private void loadFiles(){
         try {
-            this.background = new Sound(new java.io.File("assets/kateye_2.wav"));
+            this.background = new Sound(new java.io.File("assets/sounds/kateye_2.wav"));
+            this.footsteps = new Sound(new java.io.File("assets/sounds/footsteps2.wav"));
+            this.gunshot = new Sound(new java.io.File("assets/sounds/gunshot.wav"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        this.footsteps.addLineListener((lineEvent) -> {
+            if (lineEvent.getType().equals(LineEvent.Type.STOP) && this.playing_footsteps){
+                this.footsteps.stop();
+                this.footsteps.play();
+            }
+        });
+
+        this.background.addLineListener((lineEvent) -> {
+            if (lineEvent.getType().equals(LineEvent.Type.STOP)){
+                this.background.stop();
+                this.background.play();
+            }
+        });
+
+        footsteps.setVolume(1);
+        footsteps.setGain(30);
     }
 
     @Override
     public void update(Event event) {
         switch (event.getType()){
+            case MOVE:
+                if (((Move)event.getContext()).getActor() instanceof Player){
+                    if (!this.footsteps.playing()){
+                        this.footsteps.play();
+                    }
+                    this.playing_footsteps = true;
+                }
+            case STOP:
+                if (event.getContext() instanceof Player && this.footsteps.playing()){
+                    this.playing_footsteps = false;
+                    this.footsteps.stop();
+                }
             case PHYSICS_COLLISION:
                 break;
             case INITIAL_LOAD:
