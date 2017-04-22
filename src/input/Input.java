156e4761a1b493e6fca33b8313b9423fdc1ed04d
mgoo <andrewmcghienz@gmail.com>
2017-04-22 23:26:33 +1200

    added support for the new commands

--- src/input/Input.java
+++ src/input/Input.java
@@ -2,21 +2,28 @@ package input;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 import events.Event;
 import events.Events;
-import main.Globals;
-import main.Subject;
-import main.UserActions;
+import main.*;
 import physics.Move;
 import physics.Physics;
 import world.World;
 
-public class Input extends UserActions{
+public class Input extends UserActions implements Observerable{
 	private Subject subject;
+	private Map<Integer, Boolean> keyPressedMap = new HashMap<>();
+	private Map<Integer, Command> keyBindings = new HashMap<>();
 	
 	public Input(Physics physics){
 		super();
+		keyBindings.put(KeyEvent.VK_W, new Move(0, -0.5));
+		keyBindings.put(KeyEvent.VK_A, new Move(-0.5, 0));
+		keyBindings.put(KeyEvent.VK_S, new Move( 0, 0.5));
+		keyBindings.put(KeyEvent.VK_D, new Move( 0.5, 0));
+
 		this.subject = new Subject() {
 			@Override
 			protected void initObservers() {
@@ -28,17 +35,19 @@ public class Input extends UserActions{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		System.out.println("Recived keyTyped");
-		subject.notify(Events.moveEvent(new Move(Globals.player, 10, 10)));
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-
+		if (!this.keyBindings.keySet().contains(e.getKeyCode()))return;
+		this.keyPressedMap.put(e.getKeyCode(), true);
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-
+		if (!this.keyBindings.keySet().contains(e.getKeyCode()))return;
+		this.keyPressedMap.put(e.getKeyCode(), false);
 	}
 
 	@Override
@@ -64,5 +73,33 @@ public class Input extends UserActions{
 	@Override
 	public void mouseExited(MouseEvent e) {
 
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e){
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e){
+
+	}
+
+	@Override
+	public void update(Event event) {
+		switch (event.getType()){
+			case TICK:
+				for (Integer key : this.keyPressedMap.keySet()){
+					if (this.keyPressedMap.get(key)){
+						subject.notifyObservers(Events.newMoveEvent((Move)this.keyBindings.get(key)));
+					}
+				}
+				break;
+			case LOAD:
+				this.keyBindings.values().stream().forEach((value) -> {
+					value.setActor(((World)event.getContext()).getEntitiesWithType("Player").iterator().next());
+				});
+				break;
+		}
 	}
 }
