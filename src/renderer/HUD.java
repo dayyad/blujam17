a5181141fa7011168722bb53069fb566849c8543
mgoo <andrewmcghienz@gmail.com>
2017-04-23 15:20:26 +1200

    updated level

---
+++ src/renderer/HUD.java
@@ -0,0 +1,7 @@
+package renderer;
+
+/**
+ * Created by mgoo on 23/04/17.
+ */
+public class HUD {
+}
---
+++ src/world/StaticSprite.java
@@ -0,0 +1,7 @@
+package world;
+
+/**
+ * Created by mgoo on 23/04/17.
+ */
+public class StaticSprite {
+}
